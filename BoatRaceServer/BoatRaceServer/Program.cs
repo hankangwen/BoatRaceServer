64eaae00482074dd5a861ca7a1b4295c62f66440
hankangwen <13538898378@163.com>
2022-09-28 14:11:09 +0800

    Create repository.

---
+++ BoatRaceServer/BoatRaceServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoatRaceServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+        }
+    }
+}
